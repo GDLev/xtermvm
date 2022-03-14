097c5e3b0900ee4b34df3d0b6e99c1bc3c049467
GDLev <58748440+GDLev@users.noreply.github.com>
2022-03-14 14:05:57 +0100

    Create xtermvm.sh

---
+++ xtermvm.sh
@@ -0,0 +1,22 @@
+echo "please enter the ID of your virtual machine:"
+read id
+
+echo "setting up serial socket..."
+qm set $id -serial0 socket
+echo "setting up serial socket... Done"
+echo "changing grub settings and serial ports..."
+qm guest exec $id -- sh -c 'echo 'GRUB_CMDLINE_LINUX="quiet console=tty0 console=ttyS0, 115200"' >> /etc/default/grub'
+qm guest exec $id -- update-grub
+qm guest exec $id -- systemctl enable serial-getty@ttyS0.service
+qm guest exec $id -- systemctl start serial-getty@ttyS0.service
+echo "changing grub settings and serial ports... Done"
+read -p "VM ($id) reboot required! Do it now? (y/n)" agree
+if test "$agree" = "y"
+then
+     qm guest exec $id -- reboot
+     echo "Done"
+     echo "installation complete"
+else
+     echo "installation complete"	
+fi
+echo "the introduced changes may not be visible until the proxmox panel is restarted"
