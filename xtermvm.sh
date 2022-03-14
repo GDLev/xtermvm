ed57f9fec2fc0ab971a0c541447b92ea221414aa
GDLev <58748440+GDLev@users.noreply.github.com>
2022-03-14 17:50:42 +0100

    Update xtermvm.sh

--- xtermvm.sh
+++ xtermvm.sh
@@ -1,19 +1,34 @@
-echo "please enter the ID of your virtual machine:"
-read id
+
+echo 
+"
+#################################################
+#  xTerm terminal for Proxmox VMs               #
+#  By GDLev                                     #
+#                                               #
+#  Requirements:                                #
+#  - QEMU Guest agent (on VM)                   #
+#  - Serial port 0 (on VM)                      #
+#  - Serial port 0 (on VM)                      #
+#  - Linux OS                                   #
+#################################################
+"
+
+
+read -p "please enter the ID of your virtual machine:" vm
 
 echo "setting up serial socket..."
-qm set $id -serial0 socket
+qm set $vm -serial0 socket
 echo "setting up serial socket... Done"
 echo "changing grub settings and serial ports..."
-qm guest exec $id -- sh -c 'echo 'GRUB_CMDLINE_LINUX="quiet console=tty0 console=ttyS0, 115200"' >> /etc/default/grub'
-qm guest exec $id -- update-grub
-qm guest exec $id -- systemctl enable serial-getty@ttyS0.service
-qm guest exec $id -- systemctl start serial-getty@ttyS0.service
+qm guest exec $vm -- sh -c 'echo 'GRUB_CMDLINE_LINUX="quiet console=tty0 console=ttyS0, 115200"' >> /etc/default/grub'
+qm guest exec $vm -- update-grub
+qm guest exec $vm -- systemctl enable serial-getty@ttyS0.service
+qm guest exec $vm -- systemctl start serial-getty@ttyS0.service
 echo "changing grub settings and serial ports... Done"
-read -p "VM ($id) reboot required! Do it now? (y/n)" agree
+read -p "VM ($vm) reboot required! Do it now? (y/n)" agree
 if test "$agree" = "y"
 then
-     qm guest exec $id -- reboot
+     qm guest exec $vm -- reboot
      echo "Done"
 fi
 
