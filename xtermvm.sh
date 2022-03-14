b48e46f2a78a7ace796e9de594cef5b18102d418
GDLev <58748440+GDLev@users.noreply.github.com>
2022-03-14 18:05:16 +0100

    Update xtermvm.sh

--- xtermvm.sh
+++ xtermvm.sh
@@ -1,14 +1,17 @@
 
 echo 
-"#################################################
-#  xTerm terminal for Proxmox VMs By GDLev      #
+"
+#################################################
+#  xTerm terminal for Proxmox VMs               #
+#  By GDLev                                     #
 #                                               #
 #  Requirements:                                #
 #  - QEMU Guest agent (on VM)                   #
 #  - Serial port 0 (on VM)                      #
 #  - Serial port 0 (on VM)                      #
 #  - Linux OS                                   #
-#################################################"
+#################################################\n\n
+"
 
 
 read -p "please enter the ID of your virtual machine: " vm
@@ -22,7 +25,7 @@ qm guest exec $vm -- update-grub
 qm guest exec $vm -- systemctl enable serial-getty@ttyS0.service
 qm guest exec $vm -- systemctl start serial-getty@ttyS0.service
 echo "changing grub settings and serial ports... Done"
-read -p "VM ($vm) reboot required! Do it now? (y/n)" agree
+read -p "VM ($vm) reboot required! Do it now? (y/n): " agree
 if test "$agree" = "y"
 then
      qm guest exec $vm -- reboot
